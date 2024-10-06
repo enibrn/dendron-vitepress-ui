d34ca0144e00244bdb1d35f7d3d9b6a632b227a7
Andrea Binelli <binelli.andrea@gmail.com>
2024-10-06 15:49:37 +0200

    refactor stuff

--- .vitepress/config/index.mjs
+++ .vitepress/config/index.mjs
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vitepress';
 import { base } from "./base.mjs";
-import { it } from "./it.mjs";
 
 export default defineConfig({
-  ...base,
-  ...it,
-  themeConfig: {
-    ...base.themeConfig,
-    ...it.themeConfig,
-    search: {
-      ...base.themeConfig.search,
-      ...it.themeConfig.search,
-    }
-  }
+  ...base
 });
